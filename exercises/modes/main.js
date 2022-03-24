c2608c6081c10f20f8459fb005af58bb4bd164bc
sants632 <sants632@newschool.edu>
2022-03-24 13:38:07 -0400

    Updated Index

--- exercises/modes/main.js
+++ exercises/modes/main.js
@@ -45,7 +45,6 @@ function topFunction() {
 const setBg = () => {
   const randomColor = Math.floor(Math.random()*16777215).toString(16);
   document.body.style.backgroundColor = "#" + randomColor;
-  // color.innerHTML = "#" + randomColor;
 }
 
 genNew.addEventListener("click", setBg);
